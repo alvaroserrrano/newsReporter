7e23a1cfa31ce2db00f3c4d591a5aff7c597590f
Alvaro <alvaro.serrano@wmich.edu>
2020-01-02 09:51:08 +0100

    time mapper

---
+++ src/dev-data/index.js
@@ -0,0 +1,17 @@
+export const singleStory = {
+  by: 'Alvaro Serrano',
+  id: 1,
+  time: 1567209822,
+  title: 'I am a test story',
+  url: 'https://something.com/newsreporter'
+};
+
+export const storyIds = [1];
+
+export const emptySingleStory = {
+  by: undefined,
+  id: undefined,
+  time: undefined,
+  title: undefined,
+  url: undefined
+};
